b999897836e14811559ea96a18e5ecb565c02980
Golmoo <shs5991@gmail.com>
2022-11-11 16:58:24 +0900

    Add gitignore

---
+++ src/main/java/com/golmoo/springboot/web/HelloController.java
@@ -0,0 +1,15 @@
+package com.golmoo.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 준다
+public class HelloController {
+
+    @GetMapping
+    // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어줌
+    public String hello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package web;
+
+import com.golmoo.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class) // 1번
+@WebMvcTest(controllers = HelloController.class) //2번
+public class HelloControllerTest {
+
+    @Autowired  //3번
+    private MockMvc mvc;    //4번
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))  //5번
+                .andExpect(status().isOk()) //6번
+                .andExpect(content().string(hello));    //7번
+    }
+}
